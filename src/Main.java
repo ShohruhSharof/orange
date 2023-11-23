7bf41271ed6485127591bdfe693cad6e6b3a9caf
Admin <shohruh.unlimit@gmail.com>
2023-11-23 22:02:06 +0500

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Assalomu alaykum");
         Aqua aqua = new Aqua();
         try {
             aqua.start();
